05e5c307ee4bd54c53f760044388bba8c5862246
SangminL424 <xkq2643@autuni.ac.nz>
2025-08-27 12:02:18 +1200

    Add Workout, Calories, and BMI screens to bottom tabs

---
+++ FitXcel/app/(tabs)/calories.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function CaloriesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white"}}>
+      <Text>Calories Page</Text>
+    </View>
+  );
+}
